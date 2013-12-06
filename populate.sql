d57b15839a6a255a023c33eca6d5a12cab2b14d0
Jesse Bright <jbright@email.arizona.edu>
2013-12-06 12:43:43 -0700

    Populate

---
+++ populate.sql
@@ -0,0 +1,29 @@
+INSERT INTO MyNotesUser VALUES('bob@email.arizona.edu', 'Bob');
+INSERT INTO MyNotesUser VALUES('jbright@email.arizona.edu', 'Jesse');
+INSERT INTO MyNotesUser VALUES('beau.bright@gmail.com', 'Beau');
+INSERT INTO MyNotesUser VALUES('pj@yahoo.com', 'Paula');
+INSERT INTO MyNotesUser VALUES('arn91@yahoo.com', 'Tony');
+
+INSERT INTO Creation VALUES('bob@email.arizona.edu', 3);
+INSERT INTO Creation VALUES('jbright@email.arizona.edu', 4);
+INSERT INTO Creation VALUES('beau.bright@gmail.com', 1);
+INSERT INTO Creation VALUES('pj@yahoo.com', 2);
+INSERT INTO Creation VALUES('arn91@yahoo.com', 5);
+INSERT INTO Creation VALUES('pj@yahoo.com', 6);
+
+INSERT INTO Board VALUES('Gardening', 1);
+INSERT INTO Board VALUES('Movies', 2);
+INSERT INTO Board VALUES('Research', 3);
+INSERT INTO Board VALUES('Study', 4);
+INSERT INTO Board VALUES('Teaching', 5);
+INSERT INTO Board VALUES('Travel', 6);
+
+INSERT INTO Subscribes VALUES('bob@email.arizona.edu', 'Movies');
+INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Gardening');
+INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Study');
+INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Movies');
+INSERT INTO Subscribes VALUES('pj@yahoo.com', 'Travel');
+
+INSERT INTO Card VALUES('Gardening', 'Water roses', 7, 'Water all of the roses', 21, 12, 2013);
+INSERT INTO Card VALUES('Movies', 'Watch movie', 8, 'Go to scary movie', 14, 2, 2014);
+INSERT INTO Card VALUES('Research', 'Learn math', 9, 'Try to learn math'
