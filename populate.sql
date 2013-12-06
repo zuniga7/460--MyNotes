1a4dea9dd5ae53325d0475e8add174c04321de55
Jesse Bright <jbright@email.arizona.edu>
2013-12-06 13:27:17 -0700

    Should be the last change to populate, but we shall see

--- populate.sql
+++ populate.sql
@@ -10,6 +10,10 @@ INSERT INTO Creation VALUES('beau.bright@gmail.com', 1);
 INSERT INTO Creation VALUES('pj@yahoo.com', 2);
 INSERT INTO Creation VALUES('arn91@yahoo.com', 5);
 INSERT INTO Creation VALUES('pj@yahoo.com', 6);
+INSERT INTO Creation VALUES('jbright@email.arizona.edu', 7);
+INSERT INTO Creation VALUES('bob@email.arizona.edu', 8);
+INSERT INTO Creation VALUES('bob@email.arizona.edu', 9);
+INSERT INTO Creation VALUES('arn91@yahoo.com', 10);
 
 INSERT INTO Board VALUES('Gardening', 1);
 INSERT INTO Board VALUES('Movies', 2);
@@ -18,12 +22,26 @@ INSERT INTO Board VALUES('Study', 4);
 INSERT INTO Board VALUES('Teaching', 5);
 INSERT INTO Board VALUES('Travel', 6);
 
+INSERT INTO Subscribes VALUES('beau.bright@gmail.com', 'Gardening');
+INSERT INTO Subscribes VALUES('pj@yahoo.com', 'Movies');
+INSERT INTO Subscribes VALUES('bob@email.arizona.edu', 'Research');
+INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Study');
+INSERT INTO Subscribes VALUES('arn91@yahoo.com', 'Teaching');
+INSERT INTO Subscribes VALUES('pj@yahoo.com', 'Travel');
 INSERT INTO Subscribes VALUES('bob@email.arizona.edu', 'Movies');
 INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Gardening');
-INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Study');
+INSERT INTO Subscribes VALUES('beau.bright@gmail.com', 'Teaching');
 INSERT INTO Subscribes VALUES('jbright@email.arizona.edu', 'Movies');
 INSERT INTO Subscribes VALUES('pj@yahoo.com', 'Travel');
 
 INSERT INTO Card VALUES('Gardening', 'Watering', 7, 'Water garden', 21, 12, 2013);
 INSERT INTO Card VALUES('Movies', 'Watching', 8, 'Watch different movies', 14, 2, 2014);
 INSERT INTO Card VALUES('Research', 'Learn math', 9, 'Try to learn math', 16, 5, 2015);
+INSERT INTO Card VALUES('Teaching', 'Teach class', 10, 'Teach students about a subject', 28, 9, 1992)
+
+INSERT INTO AssignedTo VALUES('Gardening', 'Watering', 'jbright@email.arizona.edu');
+INSERT INTO AssignedTo VALUES('Movies', 'Watching', 'bob@email.arizona.edu');
+INSERT INTO AssignedTo VALUES('Research', 'Learn math, 'bob@email.arizona.edu');
+INSERT INTO AssignedTo VALUES('Teaching', 'Teach class', 'arn91@yahoo.com');
+INSERT INTO AssignedTo VALUES('Gardening', 'Watering', 'beau.bright@gmail.com');
+INSERT INTO AssignedTo VALUES('Movies', 'Watching', 'pj@yahoo.com');
